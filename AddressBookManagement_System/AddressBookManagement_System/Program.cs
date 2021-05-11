014d0349ba6dcd6193a5cf4bef36f5bab3e5c58d
Ketha Charan <charan.ketha1747@gmail.com>
2021-05-11 14:22:39 -0700

    [Charan] Add: User Detaials Enter And Display

---
+++ AddressBookManagement_System/AddressBookManagement_System/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace AddressBookManagement_System
+{
+    class Program
+    {
+        int PinCode;
+        long Phone_No;
+        String first_name, last_name, address, city, state;
+        public void Enter()
+        {
+            Console.Write("Enter First Name: ");
+            first_name = (Console.ReadLine());
+            Console.Write("Enter Last Name: ");
+            last_name = (Console.ReadLine());
+            Console.Write("Enter Address: ");
+            address = (Console.ReadLine());
+            Console.Write("Enter City: ");
+            city = (Console.ReadLine());
+            Console.Write("Enter State: ");
+            state = (Console.ReadLine());
+            Console.Write("Enter Pincode: ");
+            PinCode = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter Phone Number: ");
+            Phone_No = Convert.ToInt64(Console.ReadLine());
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("" + first_name + "\t" + last_name + "\t" + address + "\t" + city + "\t" + state + "\t" + PinCode + "\t" + Phone_No);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Contact Details Are ");
+            Program details = new Program();
+            details.Enter();
+            details.Show();
+        }
+    }
+}
---
+++ SumOfTwoNumbers/SumOfTwoNumbers/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("SumOfTwoNumbers")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SumOfTwoNumbers")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("921eb2e7-4c4d-44f7-a12b-3cf870c8040c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
